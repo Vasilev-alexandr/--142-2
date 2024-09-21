a63af080f46a7e913d7f0884c30f01974cc322c8
Vasilev-alexandr <vasludvladic@mail.ru>
2024-09-21 18:59:30 +0300

    Исправил 2 задание

--- 2 zadanie/list/Solver/CircularLinkedList.cpp
+++ 2 zadanie/list/Solver/CircularLinkedList.cpp
@@ -1,6 +1,19 @@
 #include "CircularLinkedList.h"
 
-CircularLinkedList::CircularLinkedList() : tail(nullptr) {}
+CircularLinkedList::CircularLinkedList() : head(nullptr), tail(nullptr) {}
+
+
+CircularLinkedList::CircularLinkedList(const CircularLinkedList& other)
+{
+    head = tail = nullptr;
+    Node* current = other.head;
+    if (current) {
+        do {
+            add(current->data);
+            current = current->next;
+        } while (current != other.head);
+    }
+}
 
 CircularLinkedList::CircularLinkedList(std::initializer_list<int> init) : tail(nullptr)
 {
@@ -10,12 +23,27 @@ CircularLinkedList::CircularLinkedList(std::initializer_list<int> init) : tail(n
     }
 }
 
-CircularLinkedList::~CircularLinkedList() {}
+CircularLinkedList::~CircularLinkedList()
+{
+    if (tail == nullptr) {
+        return;
+    }
+
+    Node* current = tail->next;
+    Node* nextNode;
+
+    do {
+        nextNode = current->next;
+        current = nextNode;
+    } while (current != tail->next);
+
+    tail = nullptr;
+}
 
 void CircularLinkedList::add(int value)
 {
     Node* newNode = new Node(value);
-    if (!tail)
+    if (tail != nullptr)
     {
         tail = newNode;
         tail->next = tail;
@@ -35,20 +63,35 @@ bool CircularLinkedList::isEmpty() const
 
 std::string CircularLinkedList::toString() const
 {
-    if (isEmpty()) return "[]";
-    std::string result = "[";
+    std::stringstream buffer;
+
+    if (tail == nullptr) {
+        return "[]";
+    }
+
+    buffer << "[";
     Node* current = tail->next;
-    do
-    {
-        result += std::to_string(current->data) + " ";
+
+    do {
+        buffer << current->data;
         current = current->next;
+
+        if (current != tail->next) {
+            buffer << " ";
+        }
     } while (current != tail->next);
-    result += "]";
-    return result;
+
+    buffer << "]";
+    return buffer.str();
 }
 
 CircularLinkedList& CircularLinkedList::operator=(const CircularLinkedList& other)
 {
+    if (this == &other)
+    {
+        return *this;
+    }
+
     return *this;
 }
 
@@ -59,6 +102,31 @@ CircularLinkedList& CircularLinkedList::operator<<(int value)
 }
 
 CircularLinkedList& CircularLinkedList::operator>>(int& value)
+
 {
+    if (head)
+    {
+        value = head->data;
+    }
+    return *this;
+}
+
+
+CircularLinkedList::CircularLinkedList(CircularLinkedList&& other) noexcept : head(other.head), tail(other.tail)
+{
+    other.head = other.tail = nullptr;
+}
+
+
+CircularLinkedList& CircularLinkedList::operator=(CircularLinkedList&& other) noexcept
+{
+    if (this == &other)
+    {
+        return *this;
+    }
+    head = other.head;
+    tail = other.tail;
+    other.head = other.tail = nullptr;
+
     return *this;
 }
